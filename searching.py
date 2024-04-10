e7d2564b5438956d80cd4d3794ca273f01bccaec
Tomas Vicar <vicar@vutbr.cz>
2024-04-10 09:34:08 +0200

    linear search

--- searching.py
+++ searching.py
@@ -1,23 +1,31 @@
 import os
-
-# get current working directory path
-cwd_path = os.getcwd()
-
-
+import json
 
 def read_data(file_name, field):
-    """
-    Reads json file and returns sequential data.
-    :param file_name: (str), name of json file
-    :param field: (str), field of a dict to return
-    :return: (list, string),
-    """
-    file_path = os.path.join(cwd_path, file_name)
+    with open(file_name) as data_file:
+        data = json.load(data_file)
+        print(data)
+    if field in data.keys():
+        return data[field]
+    else:
+        return None
 
+data = read_data("sequential.json", "unordered_numbers")
 
-def main():
-    pass
+# print(data)
 
+def linear_search(numbers, searched_number):
+    nalezene_pozice = []
+    for pozice, number in enumerate(numbers):
+        if number == searched_number:
+            nalezene_pozice.append(pozice)
+    vysledek = dict()
+    vysledek["positions"] = nalezene_pozice
+    vysledek["count"] = len(nalezene_pozice)
 
-if __name__ == '__main__':
-    main()
+    return vysledek
+
+    # for pozice in range(len(numbers)):
+
+print(data)
+print(linear_search(data, 2))
